0203801e669998b3b6a0a1947265acab7a225e4d
valentinnegut <vnegut@gmail.com>
2023-11-10 15:55:41 -0800

    Test directions given specific initial state

--- test_puzzle_solver.py
+++ test_puzzle_solver.py
@@ -1,10 +1,14 @@
-import pytest
-
-from puzzle_solver import solve_puzzle, get_directions, get_user_input
+from puzzle_solver import solve_puzzle, get_directions
 
 
-def test_directions_for_specific_input(monkeypatch):
-    initial_state = [[1, 2, 3], [4, 0, 5], [6, 7, 8]]  # Add a specific initial state here
+def test_specific_initial_state():
+    # Define the specific initial state
+    initial_state = [[1, 2, 3], [4, 0, 5], [6, 7, 8]]
+
+    # Ensure the solution is correct
+    solution = solve_puzzle(initial_state)
+    directions = get_directions(initial_state, solution)
+
     expected_directions = [
         "left",
         "down",
@@ -36,30 +40,5 @@ def test_directions_for_specific_input(monkeypatch):
         "right",
     ]
 
-    # Mock user input for get_user_input
-    monkeypatch.setattr("builtins.input", lambda _: "1 2 3 4 0 5 6 7 8")
-
-    # Redirect standard output to capture the printed directions
-    directions = get_directions(initial_state, solve_puzzle(initial_state))
     assert directions == expected_directions
 
-
-def test_get_user_input_valid(monkeypatch):
-    user_input = "1 2 3 4 5 6 7 8 0"
-    expected_output = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-
-    monkeypatch.setattr("builtins.input", lambda _: user_input)
-
-    assert get_user_input() == expected_output
-
-
-def test_get_user_input_invalid(monkeypatch):
-    user_input = "1 2 3 4 5 6 7 8"  # Missing the last element
-
-    monkeypatch.setattr("builtins.input", lambda _: user_input)
-
-    assert get_user_input() is None
-
-
-if __name__ == "__main__":
-    pytest.main()
